1cdb8cc530129a8237dd3d33423d800a7f74b79a
cktang88 <arctangent20@yahoo.com>
2018-10-31 11:38:14 -0500

    Separate js file

---
+++ sites/main.js
@@ -0,0 +1,44 @@
+const txtFile = new XMLHttpRequest();
+let public = [];
+let private = [];
+txtFile.open("GET", "sites.csv", true);
+txtFile.onreadystatechange = function() {
+    if (txtFile.readyState !== 4)  // Makes sure the document is ready to parse.
+        return;
+    if (txtFile.status !== 200)  // Makes sure it's found the file.
+        return;
+    allText = txtFile.responseText;
+    lines = txtFile.responseText.split("\r\n"); // Will separate each line into an array
+    lines = lines.map (e => e.toLowerCase().trim()); // clean a bit
+    lines = lines.sort(); // sort
+
+    let ul = document.createElement('ul');
+    lines.forEach(e =>  {
+        if (e.endsWith('*'))
+            private.push(e)
+        else
+            public.push(e)
+
+        // check for admin urls
+        const isAdmin = window.location.href.endsWith('#admin');
+        const isAdminURL = e.endsWith('*');
+        if (isAdmin || !isAdminURL) {
+            
+            // clean strs
+            e += '.vandyhacks.org';
+            e = e.replace('*', '');
+
+            if (isAdmin && isAdminURL)
+                e = e.bold();
+
+            let li = document.createElement('li');
+            let link = document.createElement('a');
+            link.href = 'https://' + e;
+            link.innerHTML = e;
+            li.appendChild(link);
+            ul.appendChild(li);
+        }
+    });
+    document.getElementById('main').appendChild(ul);
+}
+txtFile.send();
