66c939375573d9fe1a645311f4e48fc03e23b010
cktang88 <arctangent20@yahoo.com>
2018-10-31 13:16:16 -0500

    Some improvements

--- sites/main.js
+++ sites/main.js
@@ -3,18 +3,18 @@ let lines = [];
 let public = [];
 let private = [];
 txtFile.open("GET", "sites.csv", true);
-txtFile.onreadystatechange = function() {
-    if (txtFile.readyState !== 4)  // Makes sure the document is ready to parse.
+txtFile.onreadystatechange = function () {
+    if (txtFile.readyState !== 4) // Makes sure the document is ready to parse.
         return;
-    if (txtFile.status !== 200)  // Makes sure it's found the file.
+    if (txtFile.status !== 200) // Makes sure it's found the file.
         return;
 
     lines = txtFile.responseText.split("\n"); // Will separate each line into an array
-    lines = lines.map (e => e.toLowerCase().trim()); // clean a bit
+    lines = lines.map(e => e.toLowerCase().trim()); // clean a bit
     lines = lines.sort(); // sort
 
     let ul = document.createElement('ul');
-    lines.forEach(e =>  {
+    lines.forEach(e => {
         if (e.endsWith('*'))
             private.push(e)
         else
@@ -24,7 +24,7 @@ txtFile.onreadystatechange = function() {
         const isAdmin = window.location.href.endsWith('#admin');
         const isAdminURL = e.endsWith('*');
         if (isAdmin || !isAdminURL) {
-            
+
             // clean strs
             e += '.vandyhacks.org';
             e = e.replace('*', '');
@@ -40,3 +40,14 @@ txtFile.onreadystatechange = function() {
     document.getElementById('main').appendChild(ul);
 }
 txtFile.send();
+
+function isIE() {
+    const ua = navigator.userAgent;
+    /* MSIE used to detect old browsers and Trident used to newer ones*/
+    const is_ie = ua.indexOf("MSIE ") > -1 || ua.indexOf("Trident/") > -1;
+
+    return is_ie;
+}
+
+document.body.style.backgroundColor = isIE() ? '#ff00ff' : '#fff';
+document.body.style.fontFamily = isIE() ? '"Comic Sans MS", "Comic Sans", "Arial"' : 'Open Sans'
